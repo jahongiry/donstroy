e96a6e414f24ad52d66ccc3433fe20e2407c788e
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-03-25 13:08:21 +0500

    Add certificate logic

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
-import { MainPage } from '../../pages/main-page/MainPage'
-import { Footer } from '../footer/Footer'
-import { Header } from '../header/Header'
+import { Route, Routes } from 'react-router-dom';
+import { MainPage } from '../../pages/main-page/MainPage';
+import { AdminPage } from '../../pages/admin-page/AdminPage';
+import { Footer } from '../footer/Footer';
+import { Header } from '../header/Header';
+import StudentPage from '../../pages/student-page/StudentPage';
 
 export const Home = () => {
-	return (
-		<>
-			<Header />
-			<Routes>
-				<Route path='/' element={<MainPage />} />
-			</Routes>
-			<Footer />
-		</>
-	)
-}
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/admin' element={<AdminPage />} />
+        <Route path='/students/:id' element={<StudentPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
