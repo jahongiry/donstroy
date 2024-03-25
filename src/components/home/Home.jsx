5107fbdaa1c37b0b72774fcc4e96a5f0f45b5274
joxa_muxa <jaxongirmirhalikov06@gmail.com>
2024-03-25 15:31:05 +0500

    I fixed route in Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
-import { MainPage } from '../../pages/main-page/MainPage';
-import { AdminPage } from '../../pages/admin-page/AdminPage';
-import { Footer } from '../footer/Footer';
-import { Header } from '../header/Header';
-import StudentPage from '../../pages/student-page/StudentPage';
+import { Route, Routes } from 'react-router-dom'
+import { AdminPage } from '../../pages/admin-page/AdminPage'
+import Auth from '../../pages/auth/Auth'
+import { routes } from '../../routes'
+import { Footer } from '../footer/Footer'
+import { Header } from '../header/Header'
 
 export const Home = () => {
-
 	return (
 		<div className='content'>
 			<div>
