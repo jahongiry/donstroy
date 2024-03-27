36e4e8066bc95216077e9c7ca53065db64cc37e9
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-03-27 15:24:01 +0500

    Add main

--- src/components/students-table/StudentsTable.jsx
+++ src/components/students-table/StudentsTable.jsx
@@ -1,101 +1,104 @@
-import { useState } from 'react'
-import { BsCalendar2Date } from 'react-icons/bs'
-import { FaChalkboardTeacher } from 'react-icons/fa'
-import { IoSettingsOutline } from 'react-icons/io5'
-import { MdDriveFileRenameOutline, MdOutlineDescription } from 'react-icons/md'
-import { StudentEdit } from '../student-edit/StudentEdit'
-import { ZoomImage } from '../zoom-image/ZoomImage'
-import styles from './StudentsTable.module.css'
+import { useState } from 'react';
+import { BsCalendar2Date } from 'react-icons/bs';
+import { FaChalkboardTeacher } from 'react-icons/fa';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { MdDriveFileRenameOutline, MdOutlineDescription } from 'react-icons/md';
+import { StudentEdit } from '../student-edit/StudentEdit';
+import { ZoomImage } from '../zoom-image/ZoomImage';
+import styles from './StudentsTable.module.css';
+import { SUB_URL } from '../../urls/MainUrl';
 
 const StudentsTable = ({ data }) => {
-	const [showZoomImage, setShowZoomImage] = useState(false)
-	const [showEditModal, setShowEditModal] = useState(false)
-	const [img, setImg] = useState(0)
+  const [showZoomImage, setShowZoomImage] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [img, setImg] = useState(0);
 
-	const handleClick = id => {
-		setImg(id)
-		setShowEditModal(true)
-	}
+  console.log(data);
 
-	return (
-		<div className={styles.table_container}>
-			<table className={styles.table}>
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>
-							Student name
-							<MdDriveFileRenameOutline />
-						</th>
-						<th>
-							Course name
-							<MdDriveFileRenameOutline />
-						</th>
-						<th>
-							Description
-							<FaChalkboardTeacher />
-						</th>
-						<th>
-							Date
-							<BsCalendar2Date />
-						</th>
-						<th>
-							Certificate
-							<MdOutlineDescription />
-						</th>
-						<th>
-							Actions
-							<IoSettingsOutline />
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{data.map(item => (
-						<tr key={item?.id}>
-							<td>{item?.id}</td>
-							<td>
-								{/* {`${item.studentName.split(' ')[0].slice(0, 12)}${
+  const handleClick = (id) => {
+    setImg(id);
+    setShowEditModal(true);
+  };
+
+  return (
+    <div className={styles.table_container}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>
+              Student name
+              <MdDriveFileRenameOutline />
+            </th>
+            <th>
+              Course name
+              <MdDriveFileRenameOutline />
+            </th>
+            <th>
+              Description
+              <FaChalkboardTeacher />
+            </th>
+            <th>
+              Date
+              <BsCalendar2Date />
+            </th>
+            <th>
+              Certificate
+              <MdOutlineDescription />
+            </th>
+            <th>
+              Actions
+              <IoSettingsOutline />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item?.id}>
+              <td>{item?.id}</td>
+              <td>
+                {/* {`${item.studentName.split(' ')[0].slice(0, 12)}${
 									item.studentName.split(' ')[0].length >= 12 ? '...' : ''
 								} ${item.studentName.split(' ')[1].slice(0, 1)}.`} */}
-								{item?.name}
-							</td>
-							{/* <td>{item.courseName}</td>
+                {item?.name}
+              </td>
+              {/* <td>{item.courseName}</td>
 							<td>{item.description}</td> */}
-							<td>{`${item?.created_at.split('-')[2].slice(0, 2)}-${
-								item?.created_at.split('-')[1]
-							}-${item?.created_at.split('-')[0]}`}</td>
-							<td
-								onClick={() => {
-									setShowZoomImage(true)
-									setImg(item.id)
-								}}
-							>
-								{/* <img
-									src={`https://donstroy-api-production.up.railway.app/${item?.certificate_url}`}
-									alt='Certificate'
-								/> */}
-							</td>
-							<td>
-								<button
-									className={styles.action_btn}
-									onClick={() => handleClick(item.id)}
-								>
-									Edit
-								</button>
-								<button className={styles.action_btn}>Delete</button>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-			{showZoomImage && (
-				<ZoomImage img={img} setShowZoomImage={setShowZoomImage} />
-			)}
-			{showEditModal && (
-				<StudentEdit id={img} setShowEditModal={setShowEditModal} />
-			)}
-		</div>
-	)
-}
+              <td>{`${item?.created_at.split('-')[2].slice(0, 2)}-${
+                item?.created_at.split('-')[1]
+              }-${item?.created_at.split('-')[0]}`}</td>
+              <td
+                onClick={() => {
+                  setShowZoomImage(true);
+                  setImg(item.id);
+                }}
+              >
+                <img
+                  src={`https://donstroy-api-production.up.railway.app/${item?.certificate_url}`}
+                  alt='Certificate'
+                />
+              </td>
+              <td>
+                <button
+                  className={styles.action_btn}
+                  onClick={() => handleClick(item.id)}
+                >
+                  Edit
+                </button>
+                <button className={styles.action_btn}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {showZoomImage && (
+        <ZoomImage img={img} setShowZoomImage={setShowZoomImage} />
+      )}
+      {showEditModal && (
+        <StudentEdit id={img} setShowEditModal={setShowEditModal} />
+      )}
+    </div>
+  );
+};
 
-export default StudentsTable
+export default StudentsTable;
--- src/urls/MainUrl.js
+++ src/urls/MainUrl.js
@@ -1,3 +1,3 @@
-const MAIN_URL = 'https://donstroy-api-production.up.railway.app/api/v1 '
-export const SUB_URL = 'https://donstroy-api-production.up.railway.app/ '
-export default MAIN_URL
+const MAIN_URL = 'https://donstroy-api-production.up.railway.app/api/v1';
+export const SUB_URL = 'https://donstroy-api-production.up.railway.app';
+export default MAIN_URL;
